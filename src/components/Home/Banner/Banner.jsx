88dfdea582290f3eddd746bb46e3eceebca7589c
Abdur-P-Rahim <bspirahim@gmail.com>
2023-07-09 22:41:53 +0600

    color added

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -7,9 +7,9 @@ const Banner = () => {
              <div className="relative w-full">
                     <img src='/Bg-img.JPG' className="w-full" />
                     <div className="absolute h-full flex items-center left-0 bottom-0 bg-gradient-to-r from-[#151515] to-rgba(21, 21, 21, 0)">
-                        <div className='text-white space-y-7  pr-24 ml-16 text-center'>
+                        <div className='text-white space-y-7  pr-36 ml-16 text-center'>
                             <h2 className='text-5xl font-bold'>Welcome</h2>
-                            <p className='text-5xl font-bold'>I am a Web Developer</p>
+                            <p className='text-5xl font-bold'>I am a <span className='text-primary'>Web Developer</span></p>
                             <p className='text-2xl font-bold'>based in Mirpur Dhaka</p>
                             <button>
                             <button class="primary-btn">Hire Me</button>
